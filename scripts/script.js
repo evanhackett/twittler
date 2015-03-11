a9afac37541d36e4837018e1d58029e96e392a7f
evanhackett <evanwhackett@gmail.com>
2015-03-10 18:28:11 -0700

    added a button for showing new tweets, started css styling.

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,29 @@
-$(document).ready(function(){
-  var $body = $('body');
-  $body.html('');
+function displayTweet(tweet, location) {
+  console.log(tweet);
+  var $tweet = $('<li class="tweet"></li>');
+  $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + moment(tweet.created_at).fromNow());
+  $tweet.appendTo(location);
+}
 
+function displayAll(location) {
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
-    var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + moment(tweet.created_at).fromNow());
-    $tweet.appendTo($body);
+    displayTweet(tweet, location);
     index -= 1;
   }
+}
+
+$(document).ready(function(){
+  var $tweetList = $('.tweetList');
+  //$body.html('');
+
+  displayAll($tweetList);
+
+  $('.updateButton').on('click', function() {
+    $tweetList.html('<ul></ul>');
+    displayAll($tweetList);
+  });
+
 
 });
